e1a74d549b2a6eb3e8ead30f7a7f965cadfb2596
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-14 17:56:15 +0530

    Order part finished

--- client_side/Admin_Dashboard/lib/screens/order/components/order_header.dart
+++ client_side/Admin_Dashboard/lib/screens/order/components/order_header.dart
@@ -1,3 +1,4 @@
+import 'package:admin/utility/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../../utility/constants.dart';
@@ -18,7 +19,7 @@ class OrderHeader extends StatelessWidget {
         Spacer(flex: 2),
         Expanded(child: SearchField(
           onChange: (val) {
-            //TODO: should complete call filterOrders
+            context.dataProvider.filterOrders(val);
           },
         )),
         ProfileCard()
@@ -53,7 +54,7 @@ class ProfileCard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            child: Text("Angelina Jolie"),
+            child: Text("Seller"),
           ),
           Icon(Icons.keyboard_arrow_down),
         ],
